8cf2d5782254b189328fa465be80ae626183a761
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-29 11:31:59 -0500

    Does some cleaning in the eleventy configuration file.

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,35 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss")
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 
-module.exports = config => {
+module.exports = ( eleventyConfig ) => {
+  // Eleventy plugins
+  eleventyConfig.addPlugin( pluginRss );
 
-  config.addPlugin(pluginRss)
+  // Custom collections
+  eleventyConfig.addCollection('signatures',    require( './_src/_utils/getsignatures'    ));
+  eleventyConfig.addCollection('signatureOrgs', require( './_src/_utils/getsignatureorgs' ));
+  eleventyConfig.addCollection('signatures2',   require( './_src/_utils/getsignatures2'   ));
 
-  // collections
-  config.addCollection('signatures', require('./_src/_utils/getsignatures'))
-  config.addCollection('signatureOrgs', require('./_src/_utils/getsignatureorgs'))
-  config.addCollection('signatures2', require('./_src/_utils/getsignatures2'))
   return {
     templateFormats: [
-      "md",
-      "njk",
-      "css",
-      "xml",
-      "jpg",
-      "png",
-      "ico",
-      "eot",
-      "svg",
-      "ttf",
-      "woff",
-      "woff2"
+      'md',
+      'njk',
+      'css',
+      'xml',
+      'jpg',
+      'png',
+      'ico',
+      'eot',
+      'svg',
+      'ttf',
+      'woff',
+      'woff2'
     ],
-    dataTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
     dir: {
-      input: "_src",
-      output: process.env.PWD
+      input: '_src',
+      output: process.env.PWD,
     }
   }
 }
