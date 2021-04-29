ba78d43fdbd9ef5ffde3846de99d49ce0355f608
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-29 11:57:59 -0500

    Builds the site into `_site`

--- .eleventy.js
+++ .eleventy.js
@@ -29,7 +29,7 @@ module.exports = ( eleventyConfig ) => {
     markdownTemplateEngine: 'njk',
     dir: {
       input: '_src',
-      output: process.env.PWD,
+      output: '_site',
     }
   }
 }
