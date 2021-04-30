9c68405d2b2fd4f4b4032ef0a18c68e74571af62
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-29 21:22:16 -0500

    Updates the tailwindcss configuration with some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [
     './**/*.js',
@@ -8,6 +10,47 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: '#000',
+      white: '#fff',
+
+      amber: colors.amber,
+      blue: colors.blue,
+      cyan: colors.cyan,
+      emerald: colors.emerald,
+      fuchsia: colors.fuchsia,
+      gray: colors.coolGray,
+      green: colors.green,
+      indigo: colors.indigo,
+      'light-blue': colors.lightBlue,
+      lime: colors.lime,
+      orange: {
+        ...colors.orange,
+        1000: '#4a2008',
+      },
+      pink: {
+        ...colors.pink,
+        1000: '#460d25',
+      },
+      purple: colors.purple,
+      red: colors.red,
+      rose: colors.rose,
+      teal: colors.teal,
+      violet: colors.violet,
+      yellow: colors.yellow,
+
+      code: {
+        punctuation: '#A1E8FF',
+        tag: '#D58FFF',
+        'attr-name': '#4BD0FB',
+        'attr-value': '#A2F679',
+        string: '#A2F679',
+        highlight: 'rgba(134, 239, 172, 0.25)',
+      },
+    },
     extend: {},
   },
   variants: {
